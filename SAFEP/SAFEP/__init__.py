e005d557ab76f6489bc3bb8a7d8e05c2a8426630
Ezry <ezrysm@gmail.com>
2022-09-05 19:01:17 -0400

    updated imports

--- SAFEP/SAFEP/__init__.py
+++ SAFEP/SAFEP/__init__.py
@@ -7,3 +7,4 @@ Tools for Analyzing and Debugging (SA)FEP calculations
 __version__ = "0.1.0"
 __author__ = 'Jérôme Hénin, Ezry Santiago, Tom Joseph, Grace Brannigan'
 __credits__ = 'Rutgers University - Camden'
+__all__=['processing', 'plotting', 'fileIO', 'estimators', 'helpers']
--- SAFEP/SAFEP/helpers.py
+++ SAFEP/SAFEP/helpers.py
@@ -1,6 +1,28 @@
+# Import block
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
+import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
+
+from scipy.stats import linregress as lr
+from scipy.stats import norm
 from scipy.special import erfc
 from scipy.optimize import curve_fit as scipyFit
 from scipy.stats import skew
+
+import pandas as pd
+
+from alchemlyb.visualisation.dF_state import plot_dF_state
+from alchemlyb.parsing import namd
+from alchemlyb.estimators import BAR
+from alchemlyb.visualisation.dF_state import plot_dF_state
+from alchemlyb.visualisation import plot_convergence
+
+import re
+from tqdm import tqdm #for progress bars
+from natsort import natsorted #for sorting "naturally" instead of alphabetically
+from glob import glob #file regexes
 
 # Calculate the coefficient of determination:
 def GetRsq(X, Y, Yexpected):
@@ -10,7 +32,7 @@ def GetRsq(X, Y, Yexpected):
     R = 1-SSres/SStot
     R
  
-#Wrapper for fitting the normal CDF 
+#Wrappers
 def cumFn(x, m, s):
     r = norm.cdf(x, m, s)
     return r
