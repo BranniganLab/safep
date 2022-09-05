5923ca2f5597140a860194f031a612f905be8ee3
Ezry <ezrysm@gmail.com>
2022-09-05 17:06:42 -0400

    Create __init__.py

---
+++ SAFEP/SAFEP/__init__.py
@@ -0,0 +1,9 @@
+"""
+SAFEP
+
+Tools for Analyzing and Debugging (SA)FEP calculations
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Jérôme Hénin, Ezry Santiago, Tom Joseph, Grace Brannigan'
+__credits__ = 'Rutgers University - Camden'
