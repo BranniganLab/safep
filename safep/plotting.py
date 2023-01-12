30b584f2ddf7d05aaf262d80ac34e3ad92d207e0
Ezry <ezrysm@gmail.com>
2023-01-12 16:06:29 -0500

    removed subscript lambda from convergence plot

--- safep/plotting.py
+++ safep/plotting.py
@@ -86,7 +86,7 @@ def convergence_plot(theax, fs, ferr, bs, berr, fwdColor='#0072B2', bwdColor='#D
     theax.plot(0, finalMean, linewidth=1, color=lgndF, label='Forward Time Sampling')
     theax.plot(0, finalMean, linewidth=1, color=lgndB, linestyle='--', label='Backward Time Sampling')
     theax.set_xlabel('Fraction of Simulation Time', fontsize=fontsize)
-    theax.set_ylabel(r'Total $\rm\Delta G_{\lambda}$ (kcal/mol)', fontsize=fontsize)
+    theax.set_ylabel(r'Total $\rm\Delta G$ (kcal/mol)', fontsize=fontsize)
     theax.legend()
     return theax
     
