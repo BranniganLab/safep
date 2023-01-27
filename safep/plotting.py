Fixed axis units

safep/plotting.py
@@ -135,13 +135,13 @@ def plot_general(cumulative, cumulativeYlim, perWindow, perWindowYlim, RT, width
     eachAx.errorbar(perWindow.index, perWindow.BAR.df*RT, yerr=perWindow.BAR.ddf, marker=None, linewidth=1)
     eachAx.plot(perWindow.index, perWindow.EXP.dG_f*RT, marker=None, linewidth=1, alpha=0.5)
     eachAx.errorbar(perWindow.index, -perWindow.EXP.dG_b*RT, marker=None, linewidth=1, alpha=0.5)
-    eachAx.set(ylabel=r'$\rm\Delta G_{\lambda}$'+'\n'+r'$\left(\frac{kcal/mol}{\lambda}\right)$', ylim=perWindowYlim)
+    eachAx.set(ylabel=r'$\rm\Delta G_{\lambda}$'+'\n'+r'$\left(kcal/mol\right)$', ylim=perWindowYlim)
 
     # Second derivative plot
     ddG = np.diff(perWindow.BAR.df*RT)
     ddGAx.errorbar(cumulative.index[1:-1], ddG, marker='.')
     #ddGAx.set_xlabel(r'$\lambda$')
-    ddGAx.set_ylabel(r"$\Delta G'_\lambda \left(\frac{kcal/mol}{\lambda^2}\right)$")
+    ddGAx.set_ylabel(r"$\Delta\Delta G_\lambda \left(kcal/mol\right)$")
     ddGAx.set(ylim=(-1, 1))
 
     #Hysteresis Plots