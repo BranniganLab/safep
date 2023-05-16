4ca804c4c50a9f66f47b45342698db5192ec20f5
EzryStIago <ezrysm@gmail.com>
2023-05-16 15:51:08 -0400

    adding process TI

--- safep/__init__.py
+++ safep/__init__.py
@@ -7,10 +7,11 @@ Tools for Analyzing and Debugging (SA)FEP calculations
 __version__ = "0.1.0"
 __author__ = 'Jérôme Hénin, Ezry Santiago, Tom Joseph, Grace Brannigan'
 __credits__ = 'Rutgers University - Camden'
-__all__=['processing', 'plotting', 'fileIO', 'estimators', 'helpers', 'TI.py']
+__all__=['processing', 'plotting', 'fileIO', 'estimators', 'helpers', 'TI', 'process_TI']
 
 from .processing import *
 from .plotting import *
 from .fileIO import *
 from .estimators import *
 from .TI import *
+from .process_TI import *
