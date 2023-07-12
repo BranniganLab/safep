a8c6a2cadb4a610493540b0fcb82d2c74d2664e9
EzryStIago <ezrysm@gmail.com>
2023-07-12 11:38:04 -0400

    Added building blocks for basic notebook and build script

---
+++ Sample Notebooks/BuildingBlocks/Make_Notebooks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+nbmerge user_parameters.ipynb \
+        imports_and_constants.ipynb \
+        read_process_get_dG.ipynb \
+        generate_basic_plots.ipynb \
+        > basic.ipynb
