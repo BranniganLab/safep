d9b896cef046e58436bb8039705844e1d6fd5dd6
EzryStIago <ezrysm@gmail.com>
2024-03-22 18:25:20 -0400

    remove redundant function definition

--- safep/plotting.py
+++ safep/plotting.py
@@ -17,30 +17,7 @@ def plot_samples(theax, u_nk, color='blue', label='Raw Data'):
     theax.legend()
     return theax
 
-
-def convergence_plot(theax, fs, ferr, bs, berr, fwdColor='#0072B2', bwdColor='#D55E00', lgndF=None, lgndB=None):
-    '''
-    A revised convergence plot. Plays nicely with other plotting functions and is more customizable.
-    Arguments: theax[is] on which to plot, fs (forward estimates), ferr (forward errors), bs (backward-sampled estimates), berr (backward-sampled errors), fwdColor, bwdColor, lgndF legend forward color, lgndB legend backward color
-    Returns: theax[is]
-    '''
-    if not lgndF:
-        lgndF=fwdColor
-        lgndB=bwdColor
-        
-    theax.errorbar(np.arange(len(fs))/len(fs)+0.1, fs, yerr=ferr, marker='o', linewidth=1, color=fwdColor, markerfacecolor='white', markeredgewidth=1, markeredgecolor=fwdColor, ms=5)
-    theax.errorbar(np.arange(len(bs))/len(fs)+0.1, bs, yerr=berr, marker='o', linewidth=1, color=bwdColor, markerfacecolor='white', markeredgewidth=1, markeredgecolor=bwdColor, ms=5, linestyle='--')
-
-    theax.xaxis.set_ticks([0, 0.2, 0.4, 0.6, 0.8, 1])
-    
-    finalMean = fs[-1]
-    theax.axhline(y= finalMean, linestyle='-.', color='gray')
-    theax.plot(0, finalMean, linewidth=1, color=lgndF, label='Forward Time Sampling')
-    theax.plot(0, finalMean, linewidth=1, color=lgndB, linestyle='--', label='Backward Time Sampling')
-    
-    return theax
-
-def do_conv_plot(ax, X, fs, ferr, fwdColor, label=None):
+def do_conv_plot(ax, X, fs, ferr, fwd_color, label=None):
     '''
     A minimalist convergence plot
     Arguments: ax[is], X (the X values), fs (forward estimates), ferr (forward errors), fwdColor (the forward color), label text
@@ -50,10 +27,9 @@ def do_conv_plot(ax, X, fs, ferr, fwdColor, label=None):
     return ax
 
 
-def convergence_plot(theax, fs, ferr, bs, berr, fwdColor='#0072B2', bwdColor='#D55E00', lgndF=None, lgndB=None, fontsize=12):
+def convergence_plot(theax, fs, ferr, bs, berr, fwdColor='#0072B2', bwdColor='#D55E00', lgndF=None, lgndB=None, fontsize=12, errorbars=True):
     '''
-    Convergence plot. Does the convergence calculation and plotting.
-    Arguments: u_nk, tau (an error tuning factor), units (kT or kcal/mol), RT
+    Convergence plot. Does the convergence plotting.
     Returns: a pyplot
     '''
     if not lgndF:
@@ -63,9 +39,35 @@ def convergence_plot(theax, fs, ferr, bs, berr, fwdColor='#0072B2', bwdColor='#D
         
     lower = fs[-1]-ferr[-1]
     upper = fs[-1]+ferr[-1]
-    theax.fill_between([0,1],[lower, lower], [upper, upper], color=bwdColor, alpha=0.25)
-    theax.errorbar(np.arange(len(fs))/len(fs)+0.1, fs, yerr=ferr, marker='o', linewidth=1, color=fwdColor, markerfacecolor='white', markeredgewidth=1, markeredgecolor=fwdColor, ms=5)
-    theax.errorbar(np.arange(len(bs))/len(fs)+0.1, bs, yerr=berr, marker='o', linewidth=1, color=bwdColor, markerfacecolor='white', markeredgewidth=1, markeredgecolor=bwdColor, ms=5, linestyle='--')
+    theax.fill_between([0,1],[lower, lower], [upper, upper], color='gray', alpha=0.1)
+
+    fwdparams = {'marker':'o',
+                'linewidth':1,
+                'color':fwdColor,
+                'markerfacecolor':'white',
+                'markeredgewidth':1,
+                'markeredgecolor':fwdColor,
+                'ms':5,
+                }
+    bwdparams = {'marker':'o',
+                'linewidth':1,
+                'color':bwdColor,
+                'markerfacecolor':'white',
+                'markeredgewidth':1,
+                'markeredgecolor':bwdColor,
+                'ms':5,
+                'linestyle':'--'}
+
+    xfwd = np.arange(len(fs))/len(fs)+0.1
+    xbwd = np.arange(len(bs))/len(fs)+0.1
+
+    if errorbars:
+        theax.errorbar(xfwd, fs, yerr=ferr, **fwdparams)
+        theax.errorbar(xbwd, bs, yerr=berr, **bwdparams)
+    else:
+        theax.errorbar(xfwd, fs, **fwdparams)
+        theax.errorbar(xbwd, bs, **bwdparams)
+
 
     theax.xaxis.set_ticks([0, 0.2, 0.4, 0.6, 0.8, 1])
     
@@ -109,67 +111,126 @@ def fb_discrepancy_hist(dG_f, dG_b):
     return plt.gca()
 
 
-def plot_general(cumulative, cumulativeYlim, perWindow, perWindowYlim, RT, width=8, height=4, PDFtype='KDE', fontsize=12):
-    fig, axes = plt.subplots(3,2, sharex='col', sharey='row', gridspec_kw={'width_ratios': [2, 1]})
-    ((cumAx, del1),( eachAx, del2), (hystAx, pdfAx)) = axes
+def plot_general(cumulative,
+                 cumulativeYlim,
+                 perWindow,
+                 perWindowYlim,
+                 RT,
+                 width=8,
+                 height=4,
+                 PDFtype='KDE',
+                 fontsize=12,
+                 fig=None,
+                 axes=None,
+                 hysttype='classic',
+                 label=None,
+                 color='blue',
+                 errorbars=True):
+    '''
+    Plot the general analysis of cumulative and per-window changes in delta-G.
 
-    fig.delaxes(del1)
-    fig.delaxes(del2)
+    Arguments:
+    - cumulative: A pandas DataFrame containing the cumulative changes in delta-G.
+    - cumulativeYlim: The y-axis limits for the cumulative plot.
+    - perWindow: A pandas DataFrame containing the per-window changes in delta-G.
+    - perWindowYlim: The y-axis limits for the per-window plot.
+    - RT: The gas constant times the temperature.
+    - width: The width of the figure.
+    - height: The height of the figure.
+    - PDFtype: The type of probability density function to plot (either 'KDE' or 'Histogram').
+    - fontsize: The font size of the plot labels.
+    - fig: The figure object to use for plotting.
+    - axes: The axes objects to use for plotting.
+    - hysttype: The type of hysteresis plot to use (either 'classic' or 'lines').
+    - label: The label for the plot.
+    - color: The color of the plot.
+    - errorbars: Whether to include error bars in the per-window plot.
+
+    Returns:
+    - fig: The figure object.
+    - axes: The axes objects.
+    '''
+    if fig is None and axes is None:
+        fig, axes = plt.subplots(3,2, sharex='col', sharey='row', gridspec_kw={'width_ratios': [2, 1]})
+        ((cumul_ax, del1),( each_ax, del2), (hyst_ax, pdf_ax)) = axes
+
+        fig.delaxes(del1)
+        fig.delaxes(del2)
+    else:
+        cumul_ax, each_ax, hyst_ax, pdf_ax = axes
 
     # Cumulative change in kcal/mol
-    cumAx.errorbar(cumulative.index, cumulative.BAR.f*RT, yerr=cumulative.BAR.errors, marker=None, linewidth=1)
-    cumAx.set(ylabel=r'Cumulative $\mathrm{\Delta} G_{\lambda}$'+'\n(kcal/mol)', ylim=cumulativeYlim)
+    cumul_ax.errorbar(cumulative.index, cumulative.BAR.f*RT, yerr=cumulative.BAR.errors, marker=None, linewidth=1, label=label, color=color)
+    cumul_ax.set(ylabel=r'Cumulative $\mathrm{\Delta} G_{\lambda}$'+'\n(kcal/mol)', ylim=cumulativeYlim)
 
     # Per-window change in kcal/mol
-    eachAx.errorbar(perWindow.index, perWindow.BAR.df*RT, yerr=perWindow.BAR.ddf, marker=None, linewidth=1)
-    eachAx.plot(perWindow.index, perWindow.EXP.dG_f*RT, marker=None, linewidth=1, alpha=0.5)
-    eachAx.errorbar(perWindow.index, -perWindow.EXP.dG_b*RT, marker=None, linewidth=1, alpha=0.5)
-    eachAx.set(ylabel=r'$\mathrm{\Delta} G_\lambda$'+'\n'+r'$\left(kcal/mol\right)$', ylim=perWindowYlim)
+    if errorbars:
+        each_ax.errorbar(perWindow.index, perWindow.BAR.df*RT, yerr=perWindow.BAR.ddf, marker=None, linewidth=1, color=color)
+        each_ax.errorbar(perWindow.index, -perWindow.EXP.dG_b*RT, marker=None, linewidth=1, alpha=0.5, linestyle='--', color=color)
+    each_ax.plot(perWindow.index, perWindow.EXP.dG_f*RT, marker=None, linewidth=1, alpha=0.5, color=color)
+
+    each_ax.set(ylabel=r'$\mathrm{\Delta} G_\lambda$'+'\n'+r'$\left(kcal/mol\right)$', ylim=perWindowYlim)
 
     #Hysteresis Plots
-    diff = perWindow.EXP['difference']
-    hystAx.vlines(perWindow.index, np.zeros(len(perWindow)), diff, label="fwd - bwd", linewidth=2)
-    hystAx.set(ylabel=r'$\delta_\lambda$ (kcal/mol)', ylim=(-1,1))
-    hystAx.set_xlabel(xlabel=r'$\lambda$', fontsize=fontsize)
-    
-    if PDFtype=='KDE':
-        kernel = sp.stats.gaussian_kde(diff)
-        pdfX = np.linspace(-1, 1, 1000)
-        pdfY = kernel(pdfX)
-        pdfAx.plot(pdfY, pdfX, label='KDE')
-    elif PDFtype=='Histogram':
-        pdfY, pdfX = np.histogram(diff, density=True)
-        pdfX = pdfX[:-1]+(pdfX[1]-pdfX[0])/2
-        pdfAx.plot(pdfY, pdfX,  label="Estimated Distribution")
-    else:
-        raise(f"Error: PDFtype {PDFtype} not recognized")
-    
-    pdfAx.set_xlabel(PDFtype, fontsize=fontsize)
-
-    std = np.std(diff)
-    mean = np.average(diff)
-    temp = pd.Series(pdfY, index=pdfX)
-    mode = temp.idxmax()
-    
-    textstr = r"$\rm mode=$"+f"{np.round(mode,2)}"+"\n"+fr"$\mu$={np.round(mean,2)}"+"\n"+fr"$\sigma$={np.round(std,2)}"
-    props = dict(boxstyle='square', facecolor='white', alpha=0.5)
-    pdfAx.text(0.15, 0.95, textstr, transform=pdfAx.transAxes, fontsize=14,
-            verticalalignment='top', bbox=props)
+    hyst_ax, pdf_ax = plot_hysteresis(hyst_ax, pdf_ax, perWindow, hysttype, color, fontsize, PDFtype)
 
     fig.set_figwidth(width)
     fig.set_figheight(height*3)
     fig.tight_layout()
-    
-    for ax in [cumAx,eachAx,hystAx,pdfAx]:
+
+    for ax in [cumul_ax,each_ax,hyst_ax,pdf_ax]:
         ax.set_ylabel(ax.get_ylabel(), fontsize=fontsize)
 
-    return fig, [cumAx,eachAx,hystAx,pdfAx] 
+    return fig, [cumul_ax,each_ax,hyst_ax,pdf_ax] 
 
 
-'''
-This is a deprecated plotting function
-'''
+def plot_hysteresis(hyst_ax, pdf_ax, per_window, hysttype, color='blue', fontsize=12, pdf_type='KDE'):
+    diff = per_window.EXP['difference']
+    assert hysttype in ['classic', 'lines'], f"ERROR: I don't know how to plot hysttype={hysttype}"
+    if hysttype == 'classic':
+        hyst_ax.vlines(per_window.index, np.zeros(len(per_window)), diff, label="fwd - bwd", linewidth=2, color=color)
+    elif hysttype == 'lines':
+        hyst_ax.plot(per_window.index, diff, label="fwd - bwd", linewidth=1, color=color)
+
+    ytxt = r'$\delta_\lambda$ (kcal/mol)'
+    hyst_ax.set_ylabel(ylabel=ytxt, fontsize=fontsize)
+    hyst_ax.set_ylim((-1,1))
+
+    xtxt = r'$\lambda$'
+    hyst_ax.set_xlabel(xlabel=xtxt, fontsize=fontsize)
+
+    if pdf_type=='KDE':
+        kernel = sp.stats.gaussian_kde(diff)
+        pdf_x = np.linspace(-1, 1, 1000)
+        pdf_y = kernel(pdf_x)
+        pdf_ax.plot(pdf_y, pdf_x, label='KDE', color=color)
+    elif pdf_type=='Histogram':
+        pdf_y, pdf_x = np.histogram(diff, density=True)
+        pdf_x = pdf_x[:-1]+(pdf_x[1]-pdf_x[0])/2
+        pdf_ax.plot(pdf_y, pdf_x,  label="Estimated Distribution", color=color)
+    else:
+        raise f"Error: PDFtype {pdf_type} not recognized"
+
+    pdf_ax.set_xlabel(pdf_type, fontsize=fontsize)
+
+    std = np.std(diff)
+    mean = np.average(diff)
+    temp = pd.Series(pdf_y, index=pdf_x)
+    mode = temp.idxmax()
+
+    textstr = fr"$\rm mode=${np.round(mode,2)}"+"\n"+fr"$\mu$={np.round(mean,2)}"+"\n"+fr"$\sigma$={np.round(std,2)}"
+    props = dict(boxstyle='square', facecolor='white', alpha=0.5)
+    pdf_ax.text(0.15, 0.95, textstr, transform=pdf_ax.transAxes, fontsize=14,
+            verticalalignment='top', bbox=props)
+
+    return hyst_ax, pdf_ax
+
+
+
 def plot_general_legacy(cumulative, cumulativeYlim, perWindow, perWindowYlim, RT, width=8, height=4, PDFtype='KDE', fontsize=12):
+    '''
+    This is a deprecated plotting function
+    '''
     fig, axes = plt.subplots(4,2, sharex='col', sharey='row', gridspec_kw={'width_ratios': [2, 1]})
     ((cumAx, del1),( eachAx, del2), (ddGAx, del3), (hystAx, pdfAx)) = axes
 
