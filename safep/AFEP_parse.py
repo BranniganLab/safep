49f587f8a706a3d02d83b53b19d1a523d31c505c
EzryStIago <ezrysm@gmail.com>
2024-11-19 12:00:28 -0500

    replace core with BAR estimator core

--- safep/AFEP_parse.py
+++ safep/AFEP_parse.py
@@ -15,21 +15,35 @@ from alchemlyb.parsing import namd
 from alchemlyb.estimators import BAR
 from alchemlyb.visualisation.dF_state import plot_dF_state
 from alchemlyb.visualisation import plot_convergence
+from safep.processing import read_and_process
 
 import re
 
 
+import safep
+import alchemlyb
+from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from alchemlyb.parsing import namd
+from IPython.display import display, Markdown
+from pathlib import Path
+from dataclasses import dataclass
+import scipy as sp
+from alchemlyb.estimators import BAR
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-
+import argparse
+import os
+import pandas as pd
 if __name__ == '__main__':
-
-    import argparse
-    import os
-  
     parser = argparse.ArgumentParser(description='')
 
     parser.add_argument('--path', type=str, help='The absolute path to the directory containing the fepout files', default='.')
     parser.add_argument('--fepoutre', type=str, help='A regular expression that matches the fepout files of interest.', default='*.fep*')
+    parser.add_argument('--replicare', type=str, help='A regular expression that matches the replica directories', default='Replica?')
     parser.add_argument('--temperature', type=float, help='The temperature at which the FEP was run.')
     parser.add_argument('--decorrelate', type=bool, help='Flag to determine whether or not to decorrelate the data. (1=decorrelate, 0=use all data)', default=0)
     parser.add_argument('--detectEQ', type=bool, help='Flag for automated equilibrium detection.', default=0)
@@ -39,41 +53,205 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    path = args.path
-    filename = args.fepoutre
-    maxSize = args.maxSize
-    temperature = args.temperature
-    decorrelate = (args.decorrelate==1)
-    detectEQ = (args.detectEQ==1)
-    DiscrepancyFitting = args.fittingMethod
-    
-    RT = 0.00198720650096 * temperature # ca. 0.59kcal/mol
+    dataroot = Path('.')
+    replica_pattern=args.replicare
+    replicas = dataroot.glob(replica_pattern)
+    filename_pattern=args.fepoutre
+
+    temperature = 303.15
+    RT = 0.00198720650096 * temperature
+    detectEQ = True #Flag for automated equilibrium detection
 
 
-    fepoutFiles = glob(path+filename)
-    print(f"Processing: {path+filename}")
+    colors = ['blue', 'red', 'green', 'purple', 'orange', 'violet', 'cyan']
+    itcolors = iter(colors)
 
-    totalSize = 0
-    for file in fepoutFiles:
-        totalSize += os.path.getsize(file)
-    print(f"Fepout Files: {len(fepoutFiles)}\n")
+    @dataclass
+    class FepRun:
+        u_nk:           pd.DataFrame
+        perWindow:      pd.DataFrame
+        cumulative:     pd.DataFrame
+        forward:        pd.DataFrame
+        forward_error:  pd.DataFrame
+        backward:       pd.DataFrame
+        backward_error: pd.DataFrame
+        per_lambda_convergence: pd.DataFrame
+        color: str
 
-    if (totalSize/10**9)>maxSize:
-        print(f"Error: desired targets (Total size:{np.round(totalSize/10**9, 2)}GB) exceed your max size. Either increase your maximum acceptable file size, or use the 'Extended' notebook")
-        raise
+    # # Extract key features from the MBAR fitting and get ΔG
+    # Note: alchemlyb operates in units of kT by default. We multiply by RT to convert to units of kcal/mol.
+
+    # # Read and plot number of samples after detecting EQ
 
 
-    print(f'DetectEQ: {detectEQ}')
-    print(f'Decorr: {decorrelate}')
-    u_nk, affix = readAndProcess(fepoutFiles, temperature, decorrelate, detectEQ)
+    fepruns = {}
+    for replica in replicas:
+        print(f"Reading {replica}")
+        unkpath = replica.joinpath('decorrelated.csv')
+        u_nk = None
+        if unkpath.is_file():
+            print(f"Found existing dataframe. Reading.")
+            u_nk = safep.read_UNK(unkpath)
+        else:
+            print(f"Didn't find existing dataframe at {unkpath}. Checking for raw fepout files.")
+            fepoutFiles = list(replica.glob(filename_pattern))
+            totalSize = 0
+            for file in fepoutFiles:
+                totalSize += os.path.getsize(file)
+            print(f"Will process {len(fepoutFiles)} fepout files.\nTotal size:{np.round(totalSize/10**9, 2)}GB")
+
+            if len(list(fepoutFiles))>0:
+                print("Reading fepout files")
+                fig, ax = plt.subplots()
+
+                u_nk = namd.extract_u_nk(fepoutFiles, temperature)
+                u_nk = u_nk.sort_index(axis=0, level=1).sort_index(axis=1)
+                safep.plot_samples(ax, u_nk, color='blue', label='Raw Data')
+
+                if detectEQ:
+                    print("Detecting equilibrium")
+                    u_nk = safep.detect_equilibrium_u_nk(u_nk)
+                    safep.plot_samples(ax, u_nk, color='orange', label='Equilibrium-Detected')
+
+                plt.savefig(f"./{str(replica)}_FEP_number_of_samples.pdf")
+                plt.show()
+                safep.save_UNK(u_nk, unkpath)
+            else:
+                print(f"WARNING: no fepout files found for {replica}. Skipping.")
+        
+        if u_nk is not None:
+            fepruns[str(replica)] = FepRun(u_nk, None, None, None, None, None, None, None, next(itcolors))
+            
 
 
-    u_nk = u_nk.sort_index(level=1)
-    bar = BAR()
-    bar.fit(u_nk)
-    l, l_mid, f, df, ddf, errors = get_BAR(bar)
-    changeAndError = f'\u0394G = {np.round(f.iloc[-1]*RT, 1)}\u00B1{np.round(errors[-1], 3)} kcal/mol'
-    print(changeAndError)
+    # In[ ]:
+
+
+    for key, feprun in fepruns.items():
+        u_nk = feprun.u_nk
+        feprun.perWindow, feprun.cumulative = safep.do_estimation(u_nk) #Run the BAR estimator on the fep data
+        feprun.forward, feprun.forward_error, feprun.backward, feprun.backward_error = safep.do_convergence(u_nk) #Used later in the convergence plot'
+        feprun.per_lambda_convergence = safep.do_per_lambda_convergence(u_nk)
+
+
+
+    # # Plot data
+
+    # In[ ]:
+
+
+    toprint = ""
+    dGs = []
+    errors = []
+    for key, feprun in fepruns.items():
+        cumulative = feprun.cumulative
+        dG = np.round(cumulative.BAR.f.iloc[-1]*RT, 1)
+        error = np.round(cumulative.BAR.errors.iloc[-1]*RT, 1)
+        dGs.append(dG)
+        errors.append(error)
+
+        changeAndError = f'{key}: \u0394G = {dG}\u00B1{error} kcal/mol'
+        toprint += '<font size=5>{}</font><br/>'.format(changeAndError)
+
+    toprint += '<font size=5>{}</font><br/>'.format('__________________')
+    mean = np.average(dGs)
+
+    #If there are only a few replicas, the MBAR estimated error will be more reliable, albeit underestimated
+    if len(dGs)<3:
+        sterr = np.sqrt(np.sum(np.square(errors)))
+    else:
+        sterr = np.round(np.std(dGs),1)
+    toprint += '<font size=5>{}</font><br/>'.format(f'mean: {mean}')
+    toprint += '<font size=5>{}</font><br/>'.format(f'sterr: {sterr}')
+    Markdown(toprint)
+
+
+
+    def do_agg_data(dataax, plotax):
+        agg_data = []
+        lines = dataax.lines
+        for line in lines:
+            agg_data.append(line.get_ydata())
+        flat = np.array(agg_data).flatten()
+        kernel = sp.stats.gaussian_kde(flat)
+        pdfX = np.linspace(-1, 1, 1000)
+        pdfY = kernel(pdfX)
+        std = np.std(flat)
+        mean = np.average(flat)
+        temp = pd.Series(pdfY, index=pdfX)
+        mode = temp.idxmax()
+
+        textstr = r"$\rm mode=$"+f"{np.round(mode,2)}"+"\n"+fr"$\mu$={np.round(mean,2)}"+"\n"+fr"$\sigma$={np.round(std,2)}"
+        props = dict(boxstyle='square', facecolor='white', alpha=1)
+        plotax.text(0.175, 0.95, textstr, transform=plotax.transAxes, fontsize=14,
+                verticalalignment='top', bbox=props)
+
+        return plotax
+
+
+    fig = None
+    for key, feprun in fepruns.items():
+        if fig is None:
+            fig, axes = safep.plot_general(feprun.cumulative, None, feprun.perWindow, None, RT, hysttype='lines', label=key, color=feprun.color)
+            axes[1].legend()
+        else:
+            fig, axes = safep.plot_general(feprun.cumulative, None, feprun.perWindow, None, RT, fig=fig, axes=axes, hysttype='lines', label=key, color=feprun.color)
+        #fig.suptitle(changeAndError)
+
+    # hack to get aggregate data:
+    axes[3] = do_agg_data(axes[2], axes[3])
+
+    axes[0].set_title(str(mean)+r'$\pm$'+str(sterr)+' kcal/mol')
+    axes[0].legend()
+    plt.savefig(dataroot.joinpath('FEP_general_figures.pdf'))
+
+
+    # # Plot the estimated total change in free energy as a function of simulation time; contiguous subsets starting at t=0 ("Forward") and t=end ("Reverse")
+
+
+    fig, convAx = plt.subplots(1,1)
+
+    for key, feprun in fepruns.items():
+        convAx = safep.convergence_plot(convAx, 
+                                        feprun.forward*RT, 
+                                        feprun.forward_error*RT, 
+                                        feprun.backward*RT,
+                                        feprun.backward_error*RT,
+                                        fwd_color=feprun.color,
+                                        bwd_color=feprun.color,
+                                        errorbars=False
+                                        )
+        convAx.get_legend().remove()
+
+    forward_line, = convAx.plot([],[],linestyle='-', color='black', label='Forward Time Sampling')
+    backward_line, = convAx.plot([],[],linestyle='--', color='black', label='Backward Time Sampling')
+    convAx.legend(handles=[forward_line, backward_line])
+    ymin = np.min(dGs)-1
+    ymax = np.max(dGs)+1
+    convAx.set_ylim((ymin,ymax))
+    plt.savefig(dataroot.joinpath('FEP_convergence.pdf'))
+
+    genfig = None
+    for key, feprun in fepruns.items():
+        if genfig is None:
+            genfig, genaxes = safep.plot_general(feprun.cumulative, None, feprun.perWindow, None, RT, hysttype='lines', label=key, color=feprun.color)
+        else:
+            genfig, genaxes = safep.plot_general(feprun.cumulative, None, feprun.perWindow, None, RT, fig=genfig, axes=genaxes, hysttype='lines', label=key, color=feprun.color)
+    plt.delaxes(genaxes[0])
+    plt.delaxes(genaxes[1])
+
+    genaxes[3] = do_agg_data(axes[2], axes[3])
+    genaxes[2].set_title(str(mean)+r'$\pm$'+str(sterr)+' kcal/mol')
+
+    for txt in genfig.texts:
+        print(1)
+        txt.set_visible(False)
+        txt.set_text("")
+    plt.show()
+    plt.savefig(dataroot.joinpath('FEP_perLambda_convergence.pdf'))
+
+
+
 
     if args.makeFigures == 1:
         # Cumulative change in kT
