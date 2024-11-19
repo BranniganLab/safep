e86c201a7d40d21d049a4d60b9992afd3e9b9489
EzryStIago <ezrysm@gmail.com>
2024-11-19 12:08:43 -0500

    rework plotting and printing

--- safep/AFEP_parse.py
+++ safep/AFEP_parse.py
@@ -38,15 +38,16 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import argparse
 import os
 import pandas as pd
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
 
     parser.add_argument('--path', type=str, help='The absolute path to the directory containing the fepout files', default='.')
     parser.add_argument('--fepoutre', type=str, help='A regular expression that matches the fepout files of interest.', default='*.fep*')
     parser.add_argument('--replicare', type=str, help='A regular expression that matches the replica directories', default='Replica?')
-    parser.add_argument('--temperature', type=float, help='The temperature at which the FEP was run.')
-    parser.add_argument('--decorrelate', type=bool, help='Flag to determine whether or not to decorrelate the data. (1=decorrelate, 0=use all data)', default=0)
-    parser.add_argument('--detectEQ', type=bool, help='Flag for automated equilibrium detection.', default=0)
+    parser.add_argument('--temperature', type=float, help='The temperature at which the FEP was run.', default=303.15)
+    parser.add_argument('--detectEQ', type=bool, help='Flag for automated equilibrium detection.', default=True)
     parser.add_argument('--fittingMethod', type=str, help='Method for fitting the forward-backward discrepancies (hysteresis). LS=least squares, ML=maximum likelihood Default: LS', default='LS')
     parser.add_argument('--maxSize', type=float, help='Maximum total file size in GB. This is MUCH less than the required RAM. Default: 1', default=1)
     parser.add_argument('--makeFigures', type=bool, help='Run additional diagnostics and save figures to the directory. default: False', default=0)
@@ -58,9 +59,9 @@ if __name__ == '__main__':
     replicas = dataroot.glob(replica_pattern)
     filename_pattern=args.fepoutre
 
-    temperature = 303.15
+    temperature = args.temperature
     RT = 0.00198720650096 * temperature
-    detectEQ = True #Flag for automated equilibrium detection
+    detectEQ = args.detectEQ
 
 
     colors = ['blue', 'red', 'green', 'purple', 'orange', 'violet', 'cyan']
@@ -123,22 +124,11 @@ if __name__ == '__main__':
             fepruns[str(replica)] = FepRun(u_nk, None, None, None, None, None, None, None, next(itcolors))
             
 
-
-    # In[ ]:
-
-
     for key, feprun in fepruns.items():
         u_nk = feprun.u_nk
         feprun.perWindow, feprun.cumulative = safep.do_estimation(u_nk) #Run the BAR estimator on the fep data
         feprun.forward, feprun.forward_error, feprun.backward, feprun.backward_error = safep.do_convergence(u_nk) #Used later in the convergence plot'
         feprun.per_lambda_convergence = safep.do_per_lambda_convergence(u_nk)
-
-
-
-    # # Plot data
-
-    # In[ ]:
-
 
     toprint = ""
     dGs = []
@@ -150,10 +140,10 @@ if __name__ == '__main__':
         dGs.append(dG)
         errors.append(error)
 
-        changeAndError = f'{key}: \u0394G = {dG}\u00B1{error} kcal/mol'
-        toprint += '<font size=5>{}</font><br/>'.format(changeAndError)
+        changeAndError = f'{key}: \u0394G = {dG}\u00B1{error} kcal/mol\n'
+        toprint += changeAndError
 
-    toprint += '<font size=5>{}</font><br/>'.format('__________________')
+    toprint += "\n"
     mean = np.average(dGs)
 
     #If there are only a few replicas, the MBAR estimated error will be more reliable, albeit underestimated
@@ -161,168 +151,91 @@ if __name__ == '__main__':
         sterr = np.sqrt(np.sum(np.square(errors)))
     else:
         sterr = np.round(np.std(dGs),1)
-    toprint += '<font size=5>{}</font><br/>'.format(f'mean: {mean}')
-    toprint += '<font size=5>{}</font><br/>'.format(f'sterr: {sterr}')
-    Markdown(toprint)
-
-
-
-    def do_agg_data(dataax, plotax):
-        agg_data = []
-        lines = dataax.lines
-        for line in lines:
-            agg_data.append(line.get_ydata())
-        flat = np.array(agg_data).flatten()
-        kernel = sp.stats.gaussian_kde(flat)
-        pdfX = np.linspace(-1, 1, 1000)
-        pdfY = kernel(pdfX)
-        std = np.std(flat)
-        mean = np.average(flat)
-        temp = pd.Series(pdfY, index=pdfX)
-        mode = temp.idxmax()
-
-        textstr = r"$\rm mode=$"+f"{np.round(mode,2)}"+"\n"+fr"$\mu$={np.round(mean,2)}"+"\n"+fr"$\sigma$={np.round(std,2)}"
-        props = dict(boxstyle='square', facecolor='white', alpha=1)
-        plotax.text(0.175, 0.95, textstr, transform=plotax.transAxes, fontsize=14,
-                verticalalignment='top', bbox=props)
-
-        return plotax
-
-
-    fig = None
-    for key, feprun in fepruns.items():
-        if fig is None:
-            fig, axes = safep.plot_general(feprun.cumulative, None, feprun.perWindow, None, RT, hysttype='lines', label=key, color=feprun.color)
-            axes[1].legend()
-        else:
-            fig, axes = safep.plot_general(feprun.cumulative, None, feprun.perWindow, None, RT, fig=fig, axes=axes, hysttype='lines', label=key, color=feprun.color)
-        #fig.suptitle(changeAndError)
-
-    # hack to get aggregate data:
-    axes[3] = do_agg_data(axes[2], axes[3])
-
-    axes[0].set_title(str(mean)+r'$\pm$'+str(sterr)+' kcal/mol')
-    axes[0].legend()
-    plt.savefig(dataroot.joinpath('FEP_general_figures.pdf'))
-
-
-    # # Plot the estimated total change in free energy as a function of simulation time; contiguous subsets starting at t=0 ("Forward") and t=end ("Reverse")
-
-
-    fig, convAx = plt.subplots(1,1)
-
-    for key, feprun in fepruns.items():
-        convAx = safep.convergence_plot(convAx, 
-                                        feprun.forward*RT, 
-                                        feprun.forward_error*RT, 
-                                        feprun.backward*RT,
-                                        feprun.backward_error*RT,
-                                        fwd_color=feprun.color,
-                                        bwd_color=feprun.color,
-                                        errorbars=False
-                                        )
-        convAx.get_legend().remove()
-
-    forward_line, = convAx.plot([],[],linestyle='-', color='black', label='Forward Time Sampling')
-    backward_line, = convAx.plot([],[],linestyle='--', color='black', label='Backward Time Sampling')
-    convAx.legend(handles=[forward_line, backward_line])
-    ymin = np.min(dGs)-1
-    ymax = np.max(dGs)+1
-    convAx.set_ylim((ymin,ymax))
-    plt.savefig(dataroot.joinpath('FEP_convergence.pdf'))
-
-    genfig = None
-    for key, feprun in fepruns.items():
-        if genfig is None:
-            genfig, genaxes = safep.plot_general(feprun.cumulative, None, feprun.perWindow, None, RT, hysttype='lines', label=key, color=feprun.color)
-        else:
-            genfig, genaxes = safep.plot_general(feprun.cumulative, None, feprun.perWindow, None, RT, fig=genfig, axes=genaxes, hysttype='lines', label=key, color=feprun.color)
-    plt.delaxes(genaxes[0])
-    plt.delaxes(genaxes[1])
-
-    genaxes[3] = do_agg_data(axes[2], axes[3])
-    genaxes[2].set_title(str(mean)+r'$\pm$'+str(sterr)+' kcal/mol')
-
-    for txt in genfig.texts:
-        print(1)
-        txt.set_visible(False)
-        txt.set_text("")
-    plt.show()
-    plt.savefig(dataroot.joinpath('FEP_perLambda_convergence.pdf'))
-
-
-
+    toprint += f'mean: {mean} kcal/mol\n' + f'sterr: {sterr} kcal/mol'
+    print(toprint)
 
     if args.makeFigures == 1:
-        # Cumulative change in kT
-        plt.errorbar(l, f, yerr=errors, marker='.')
-        plt.xlabel('lambda')
-        plt.ylabel('DeltaG(lambda) (kT)')
-        plt.title(f'Cumulative dG with accumulated errors {affix}\n{changeAndError}')
-        plt.savefig(f'{path}dG_cumulative_kT_{affix}.png', dpi=600)
-        plt.clf()
+        # # Plot data
+        def do_agg_data(dataax, plotax):
+            agg_data = []
+            lines = dataax.lines
+            for line in lines:
+                agg_data.append(line.get_ydata())
+            flat = np.array(agg_data).flatten()
+            kernel = sp.stats.gaussian_kde(flat)
+            pdfX = np.linspace(-1, 1, 1000)
+            pdfY = kernel(pdfX)
+            std = np.std(flat)
+            mean = np.average(flat)
+            temp = pd.Series(pdfY, index=pdfX)
+            mode = temp.idxmax()
 
-        # Per-window change in kT
-        plt.errorbar(l_mid, df, yerr=ddf, marker='.')
-        plt.xlabel('lambda')
-        plt.ylabel('Delta G per window (kT)')
-        plt.title(f'Per-Window dG with individual errors {affix}')
-        plt.savefig(f'{path}dG_{affix}.png', dpi=600)
-        plt.clf()
+            textstr = r"$\rm mode=$"+f"{np.round(mode,2)}"+"\n"+fr"$\mu$={np.round(mean,2)}"+"\n"+fr"$\sigma$={np.round(std,2)}"
+            props = dict(boxstyle='square', facecolor='white', alpha=1)
+            plotax.text(0.175, 0.95, textstr, transform=plotax.transAxes, fontsize=14,
+                    verticalalignment='top', bbox=props)
 
-        # Per-window change in kT
-        plt.errorbar(l[1:-1], np.diff(df), marker='.')
-        plt.xlabel('lambda (L)')
-        plt.ylabel("dG'(L)")
-        plt.title(f'derivative of dG {affix}')
-        plt.savefig(f'{path}dG_prime_{affix}.png', dpi=600)
-        plt.clf()
-
-        ####
-        try:
-            convergence_plot(u_nk, l)
-            plt.title(f'Convergence {affix}')
-            plt.savefig(f'{path}convergence_{affix}.png', dpi=600)
-            plt.clf()
-        except:
-            print("Failed to generate convergence plot. Probably due to too few samples after decorrelation.")
-
-        ####
-        l, l_mid, dG_f, dG_b = get_EXP(u_nk)
-        plt.vlines(l_mid, np.zeros(len(l_mid)), dG_f + np.array(dG_b), label="fwd - bwd", linewidth=2)
-
-        plt.legend()
-        plt.title(f'Fwd-bwd discrepancies by lambda {affix}')
-        plt.xlabel('Lambda')
-        plt.ylabel('Diff. in delta-G')
-        plt.savefig(f'{path}discrepancies_{affix}.png', dpi=600)
-        plt.clf()
+            return plotax
 
 
-        ###
-        #Do residual fitting
-        ###
-        X, Y, pdfX, pdfY, fitted, pdfXnorm, pdfYnorm, pdfYexpected = getPDF(dG_f, dG_b)
-        
-        #plot the data
-        fig, (pdfAx, pdfResid) = plt.subplots(2, 1, sharex=True)
-        plt.xlabel('Difference in delta-G')
-        
-        pdfAx.plot(pdfX, pdfY,  label="Estimated Distribution")
-        pdfAx.set_ylabel("PDF")
-        pdfAx.plot(pdfXnorm, pdfYnorm, label="Fitted Normal Distribution", color="orange")
+        fig = None
+        for key, feprun in fepruns.items():
+            if fig is None:
+                fig, axes = safep.plot_general(feprun.cumulative, None, feprun.perWindow, None, RT, hysttype='lines', label=key, color=feprun.color)
+                axes[1].legend()
+            else:
+                fig, axes = safep.plot_general(feprun.cumulative, None, feprun.perWindow, None, RT, fig=fig, axes=axes, hysttype='lines', label=key, color=feprun.color)
+            #fig.suptitle(changeAndError)
 
-        #pdf residuals
-        pdfResiduals = pdfY-pdfYexpected
-        pdfResid.plot(pdfX, pdfResiduals)
-        pdfResid.set_ylabel("PDF residuals") 
+        # hack to get aggregate data:
+        axes[3] = do_agg_data(axes[2], axes[3])
 
-        fig.set_figheight(10)
-        if DiscrepancyFitting == 'LS':
-            pdfAx.title.set_text(f"Least squares fitting of cdf(fwd-bkwd)\nSkewness: {np.round(skew(X),2)}\nFitted parameters: Mean={np.round(fitted[0],3)}, Stdv={np.round(fitted[1],3)}\nPopulation parameters: Mean={np.round(np.average(X),3)}, Stdv={np.round(np.std(X),3)}")
-            plt.savefig(f"{path}LeastSquares_pdf_{affix}.png", dpi=600)
-        elif DiscrepancyFitting == 'ML':
-            pdfAx.title.set_text(f"Maximum likelihood fitting of fwd-bkwd\nFitted parameters: Mean={np.round(fitted[0],3)}, Stdv={np.round(fitted[1],3)}\nPopulation parameters: Mean={np.round(np.average(X),3)}, Stdv={np.round(np.std(X),3)}")
-            plt.savefig(f"{path}MaximumLikelihood_pdf_{affix}.png", dpi=600)
-        plt.clf()
+        axes[0].set_title(str(mean)+r'$\pm$'+str(sterr)+' kcal/mol')
+        axes[0].legend()
+        plt.savefig(dataroot.joinpath('FEP_general_figures.pdf'))
+
+
+        # # Plot the estimated total change in free energy as a function of simulation time; contiguous subsets starting at t=0 ("Forward") and t=end ("Reverse")
+
+
+        fig, convAx = plt.subplots(1,1)
+
+        for key, feprun in fepruns.items():
+            convAx = safep.convergence_plot(convAx, 
+                                            feprun.forward*RT, 
+                                            feprun.forward_error*RT, 
+                                            feprun.backward*RT,
+                                            feprun.backward_error*RT,
+                                            fwd_color=feprun.color,
+                                            bwd_color=feprun.color,
+                                            errorbars=False
+                                            )
+            convAx.get_legend().remove()
+
+        forward_line, = convAx.plot([],[],linestyle='-', color='black', label='Forward Time Sampling')
+        backward_line, = convAx.plot([],[],linestyle='--', color='black', label='Backward Time Sampling')
+        convAx.legend(handles=[forward_line, backward_line])
+        ymin = np.min(dGs)-1
+        ymax = np.max(dGs)+1
+        convAx.set_ylim((ymin,ymax))
+        plt.savefig(dataroot.joinpath('FEP_convergence.pdf'))
+
+        genfig = None
+        for key, feprun in fepruns.items():
+            if genfig is None:
+                genfig, genaxes = safep.plot_general(feprun.cumulative, None, feprun.perWindow, None, RT, hysttype='lines', label=key, color=feprun.color)
+            else:
+                genfig, genaxes = safep.plot_general(feprun.cumulative, None, feprun.perWindow, None, RT, fig=genfig, axes=genaxes, hysttype='lines', label=key, color=feprun.color)
+        plt.delaxes(genaxes[0])
+        plt.delaxes(genaxes[1])
+
+        genaxes[3] = do_agg_data(axes[2], axes[3])
+        genaxes[2].set_title(str(mean)+r'$\pm$'+str(sterr)+' kcal/mol')
+
+        for txt in genfig.texts:
+            print(1)
+            txt.set_visible(False)
+            txt.set_text("")
+        plt.show()
+        plt.savefig(dataroot.joinpath('FEP_perLambda_convergence.pdf'))
 
