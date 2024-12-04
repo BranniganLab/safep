Fix silent TI bug; add decoupling option

safep/TI.py
@@ -49,8 +49,14 @@ def plot_TI(cumulative, perWindow, width=8, height=4, PDFtype='KDE', hystLim=(-1
     
     return fig, [cumAx,eachAx] 
 
-def make_harmonicWall(FC=10, targetFC=0, targetFE=1, upperWalls=1, schedule=None, numSteps=1000, targetEQ=500, name='HW', lowerWalls=None):
-    HW = {'name':name, 'targetFC':targetFC, 'targetFE':targetFE, 'FC':FC, 'upperWalls':upperWalls, 'schedule':schedule, 'numSteps':numSteps, 'targetEQ':targetEQ, 'lowerWalls':lowerWalls}
+def make_harmonicWall(FC=10, targetFC=0, targetFE=1, upperWalls=1, schedule=None, numSteps=1000, targetEQ=500, name='HW', lowerWalls=None, lambdaExp=1., decoupling=None):
+    # Heuristic to set the default value of the decoupling parameter
+    if decoupling is None:
+        decoupling = (targetFC == 0 and FC != 0)
+
+    HW = {'name':name, 'targetFC':targetFC, 'targetFE':targetFE, 'FC':FC, 'upperWalls':upperWalls, 'schedule':schedule,
+           'numSteps':numSteps, 'targetEQ':targetEQ, 'lowerWalls':lowerWalls, 'lambdaExp':lambdaExp, 'decoupling':decoupling}
+
     return HW
 
 def harmonicWall_U(HW, coord, L):
@@ -61,9 +67,14 @@ def harmonicWall_U(HW, coord, L):
         d = coord-HW['lowerWalls']
     
     if d!=0:
-        dk = HW['targetFC']-HW['FC']
-        la = L**HW['targetFE']
-        kL = HW['FC']+la*dk
+        alpha = HW['targetFE']
+        if HW['decoupling']:
+            kL = (1.-L)**alpha * HW['FC']
+        else:
+            dk = HW['targetFC']-HW['FC']
+            la = L**alpha
+            kL = HW['FC']+la*dk
+
         U = 0.5*kL*(d**2)
     else:
         U=0
@@ -75,30 +86,17 @@ def harmonicWall_dUdL(HW, coord, L):
         d = coord-HW['upperWalls']
     elif HW['lowerWalls'] and coord<HW['lowerWalls']:
         d = coord-HW['lowerWalls']
-    
+
     if d!=0:
-        dk = HW['targetFC']-HW['FC']
-        dla = HW['targetFE']*L**(HW['targetFE']-1)
-        kL = HW['FC']+dla*dk
-        dU = 0.5*kL*(d**2)
+        alpha = HW['targetFE']
+        if HW['decoupling']:
+            dkL = -alpha * (1.-L)**(alpha-1) * HW['FC']
+        else:
+            dk = HW['targetFC']-HW['FC']
+            dla = alpha*L**(alpha-1)
+            dkL = dla*dk
+
+        dU = 0.5*dkL*(d**2)
     else:
         dU=0
     return dU
-
-
-# Probably not relevant for the tutorial
-
-#if np.isnan(dataTI.E_dist.iloc[0]):
-#    dataTI.loc[:,'E_dist'] = [HW_U(Dist, coord, 0) for coord in dataTI.distance]
-#if np.isnan(dataTI.E_DBC.iloc[0]):
-#    dataTI.loc[:,'E_DBC'] = [HW_U(DBC, coord, L) for coord, L in zip(dataTI.DBC, dataTI.L)]
-
-#plt.plot(dataTI.E_dist, label='spherical restraint', alpha=0.5)
-#plt.plot(dataTI.E_DBC, label='DBC restraint', alpha=0.5)
-#plt.legend()
-#plt.savefig(f'{path}_restraint_overlap.pdf')
-
-#plt.plot(RFEPdat, label='colvars estimate', color='red')
-#plt.errorbar(Lsched, TIperWindow['dGdL'], yerr=TIperWindow['error'], label='Notebook Estimate', linestyle='-.', color='black')
-#plt.savefig(f'{path}_TI_vs_colvarEst.pdf')
-#plt.legend()