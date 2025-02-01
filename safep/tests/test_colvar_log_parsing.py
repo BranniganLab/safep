b84132432c403116e691ffd6ce440093ab077222
EzryStIago <ezrysm@gmail.com>
2025-02-01 15:49:38 -0500

    test: split tests between the four pieces that get returned by the parser

--- safep/tests/test_colvar_log_parsing.py
+++ safep/tests/test_colvar_log_parsing.py
@@ -1,9 +1,25 @@
 from approvaltests.approvals import verify
 from safep.fileIO import parse_Colvars_log
 from pathlib import Path
+import pytest
+from collections import namedtuple
 
 SAMPLE_RFEP_LOG = Path("../../Sample_Notebooks/RFEP_decouple.log")
 
-def test_parsing():
-    result = parse_Colvars_log(SAMPLE_RFEP_LOG)
-    verify(result)
+@pytest.fixture
+def parse_result():
+    ParseResult = namedtuple('ParseReult', 'global_conf colvars biases TI_traj')
+    return ParseResult(*parse_Colvars_log(SAMPLE_RFEP_LOG))
+
+def test_global_config(parse_result):
+    verify(parse_result.global_conf)
+
+def test_colvars(parse_result):
+    verify(parse_result.colvars)
+
+def test_biases(parse_result):
+    verify(parse_result.biases)
+
+def test_TI_traj(parse_result):
+    verify(parse_result.TI_traj)
+
