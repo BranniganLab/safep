d845a4e925887adfe8477c6b9d26cd6426ac9f57
EzryStIago <ezrysm@gmail.com>
2025-03-08 16:58:16 -0500

    refactor: extract summary statistics method

--- safep/AFEP_parse.py
+++ safep/AFEP_parse.py
@@ -43,7 +43,8 @@ class FepRun:
             self.backward,
             self.backward_error,
         ) = safep.do_convergence(self.u_nk)  # Used later in the convergence plot
-        self.per_lambda_convergence = safep.do_per_lambda_convergence(self.u_nk)
+        self.per_lambda_convergence = safep.do_per_lambda_convergence(
+            self.u_nk)
 
 
 def do_agg_data(dataax, plotax):
@@ -163,6 +164,7 @@ def initialize_general_figure(RT_kcal_per_mol, key, feprun):
 
     return fig, axes
 
+
 def report_number_and_size_of_fepout_files(fepoutFiles):
     total_size = 0
     for file in fepoutFiles:
@@ -208,14 +210,19 @@ def read_and_decorrelate(args, replica, unkpath, fepoutFiles):
     if args.detectEQ:
         print("Detecting equilibrium")
         u_nk = safep.detect_equilibrium_u_nk(u_nk)
-        safep.plot_samples(ax, u_nk, color="orange", label="Equilibrium-Detected")
+        safep.plot_samples(ax, u_nk, color="orange",
+                           label="Equilibrium-Detected")
 
-    plt.savefig(args.dataroot.joinpath(f"{str(replica)}_FEP_number_of_samples.pdf"))
+    plt.savefig(args.dataroot.joinpath(
+        f"{str(replica)}_FEP_number_of_samples.pdf"))
     safep.save_UNK(u_nk, unkpath)
     return u_nk
 
 
 def process_replicas(args, itcolors):
+    # # Extract key features from the MBAR fitting and get ΔG
+    # Note: alchemlyb operates in units of kT by default.
+    # We multiply by RT to convert to units of kcal/mol.
     fepruns = {}
     for replica in args.replicas:
         print(f"Reading {replica}")
@@ -225,15 +232,18 @@ def process_replicas(args, itcolors):
             print("Found existing dataframe. Reading.")
             u_nk = safep.read_UNK(unkpath)
         else:
-            print(f"Didn't find existing dataframe at {unkpath}. Checking for raw fepout files.")
+            print(
+                f"Didn't find existing dataframe at {unkpath}. Checking for raw fepout files.")
             fepoutFiles = list(replica.glob(args.filename_pattern))
             report_number_and_size_of_fepout_files(fepoutFiles)
 
             if len(list(fepoutFiles)) > 0:
                 print("Reading fepout files")
-                u_nk = read_and_decorrelate(args, replica, unkpath, fepoutFiles)
+                u_nk = read_and_decorrelate(
+                    args, replica, unkpath, fepoutFiles)
             else:
-                print(f"WARNING: no fepout files found for {replica}. Skipping.")
+                print(
+                    f"WARNING: no fepout files found for {replica}. Skipping.")
 
         if u_nk is not None:
             fepruns[str(replica)] = FepRun(u_nk, None, None, None, None, None, None, None,
@@ -243,19 +253,20 @@ def process_replicas(args, itcolors):
 
 def add_to_general_figure(fig, axes, args, key, feprun):
     fig, axes = safep.plot_general(
-                    feprun.cumulative,
-                    None,
-                    feprun.perWindow,
-                    None,
-                    args.RT_kcal_per_mol,
-                    fig=fig,
-                    axes=axes,
-                    hysttype="lines",
-                    label=key,
-                    color=feprun.color,
-                )
-    
+        feprun.cumulative,
+        None,
+        feprun.perWindow,
+        None,
+        args.RT_kcal_per_mol,
+        fig=fig,
+        axes=axes,
+        hysttype="lines",
+        label=key,
+        color=feprun.color,
+    )
+
     return fig, axes
+
 
 def add_summary_stats(do_agg_data, mean, sterr, axes):
     axes[3] = do_agg_data(axes[2], axes[3])
@@ -264,30 +275,31 @@ def add_summary_stats(do_agg_data, mean, sterr, axes):
     axes[0].legend()
     return axes
 
+
 def do_shared_convergence_plot(args, fepruns, dGs):
     fig, convAx = plt.subplots(1, 1)
 
     for key, feprun in fepruns.items():
         convAx = safep.convergence_plot(
-                convAx,
-                feprun.forward * args.RT_kcal_per_mol,
-                feprun.forward_error * args.RT_kcal_per_mol,
-                feprun.backward * args.RT_kcal_per_mol,
-                feprun.backward_error * args.RT_kcal_per_mol,
-                fwd_color=feprun.color,
-                bwd_color=feprun.color,
-                errorbars=False,
-            )
+            convAx,
+            feprun.forward * args.RT_kcal_per_mol,
+            feprun.forward_error * args.RT_kcal_per_mol,
+            feprun.backward * args.RT_kcal_per_mol,
+            feprun.backward_error * args.RT_kcal_per_mol,
+            fwd_color=feprun.color,
+            bwd_color=feprun.color,
+            errorbars=False,
+        )
         convAx.get_legend().remove()
 
     (forward_line,) = convAx.plot([], [],
-                                      linestyle="-",
-                                      color="black",
-                                      label="Forward Time Sampling")
+                                  linestyle="-",
+                                  color="black",
+                                  label="Forward Time Sampling")
     (backward_line,) = convAx.plot([], [],
-                                       linestyle="--",
-                                       color="black",
-                                       label="Backward Time Sampling")
+                                   linestyle="--",
+                                   color="black",
+                                   label="Backward Time Sampling")
     convAx.legend(handles=[forward_line, backward_line])
     ymin = np.min(dGs) - 1
     ymax = np.max(dGs) + 1
@@ -295,24 +307,26 @@ def do_shared_convergence_plot(args, fepruns, dGs):
 
     return fig, convAx
 
-def do_per_lambda_convergence_shared_axes(do_agg_data, initialize_general_figure, args, fepruns, mean, sterr, axes):
+
+def do_per_lambda_convergence_shared_axes(args, fepruns, mean, sterr, axes):
     genfig = None
     for key, feprun in fepruns.items():
         if genfig is None:
-            genfig, genaxes = initialize_general_figure(args.RT_kcal_per_mol, key, feprun)
+            genfig, genaxes = initialize_general_figure(
+                args.RT_kcal_per_mol, key, feprun)
         else:
             genfig, genaxes = safep.plot_general(
-                    feprun.cumulative,
-                    None,
-                    feprun.perWindow,
-                    None,
-                    args.RT_kcal_per_mol,
-                    fig=genfig,
-                    axes=genaxes,
-                    hysttype="lines",
-                    label=key,
-                    color=feprun.color,
-                )
+                feprun.cumulative,
+                None,
+                feprun.perWindow,
+                None,
+                args.RT_kcal_per_mol,
+                fig=genfig,
+                axes=genaxes,
+                hysttype="lines",
+                label=key,
+                color=feprun.color,
+            )
     plt.delaxes(genaxes[0])
     plt.delaxes(genaxes[1])
 
@@ -326,11 +340,13 @@ def do_per_lambda_convergence_shared_axes(do_agg_data, initialize_general_figure
 
     return genfig, genaxes
 
+
 def make_figures(args, fepruns, dGs, mean, sterr):
     fig = None
     for key, feprun in fepruns.items():
         if fig is None:
-            fig, axes = initialize_general_figure(args.RT_kcal_per_mol, key, feprun)
+            fig, axes = initialize_general_figure(
+                args.RT_kcal_per_mol, key, feprun)
         else:
             fig, axes = add_to_general_figure(fig, axes, args, key, feprun)
 
@@ -338,36 +354,28 @@ def make_figures(args, fepruns, dGs, mean, sterr):
     axes = add_summary_stats(do_agg_data, mean, sterr, axes)
     fig.savefig(args.dataroot.joinpath("FEP_general_figures.pdf"))
 
-        # # Plot the estimated total change in free energy as a function of simulation time;
-        # contiguous subsets starting at t=0 ("Forward") and t=end ("Reverse")
+    # # Plot the estimated total change in free energy as a function of simulation time;
+    # contiguous subsets starting at t=0 ("Forward") and t=end ("Reverse")
 
     fig, convAx = do_shared_convergence_plot(args, fepruns, dGs)
     fig.savefig(args.dataroot.joinpath("FEP_convergence.pdf"))
 
-    fig, axes = do_per_lambda_convergence_shared_axes(do_agg_data, initialize_general_figure, args, fepruns, mean, sterr, axes)
+    fig, axes = do_per_lambda_convergence_shared_axes(
+        args, fepruns, mean, sterr, axes)
     fig.savefig(args.dataroot.joinpath("FEP_perLambda_convergence.pdf"))
 
     plt.show()
 
-if __name__ == "__main__":
-    parser = AFEPArgumentParser()
-    args = AFEPArguments.from_AFEPArgumentParser(parser)
 
-    itcolors = iter(COLORS)
-
-    # # Extract key features from the MBAR fitting and get ΔG
-    # Note: alchemlyb operates in units of kT by default.
-    # We multiply by RT to convert to units of kcal/mol.
-
-    fepruns = process_replicas(args, itcolors)
-
+def get_summary_statistics(args, fepruns):
     toprint = ""
     dGs = []
     errors = []
     for key, feprun in fepruns.items():
         cumulative = feprun.cumulative
         dG = np.round(cumulative.BAR.f.iloc[-1] * args.RT_kcal_per_mol, 1)
-        error = np.round(cumulative.BAR.errors.iloc[-1] * args.RT_kcal_per_mol, 1)
+        error = np.round(
+            cumulative.BAR.errors.iloc[-1] * args.RT_kcal_per_mol, 1)
         dGs.append(dG)
         errors.append(error)
 
@@ -384,7 +392,19 @@ if __name__ == "__main__":
     else:
         sterr = np.round(np.std(dGs), 1)
     toprint += f"mean: {mean} kcal/mol\n" + f"sterr: {sterr} kcal/mol"
-    print(toprint)
+    return toprint, dGs, mean, sterr
+
+
+if __name__ == "__main__":
+    parser = AFEPArgumentParser()
+    args = AFEPArguments.from_AFEPArgumentParser(parser)
+
+    itcolors = iter(COLORS)
+
+    fepruns = process_replicas(args, itcolors)
+
+    summary, dGs, mean, sterr = get_summary_statistics(args, fepruns)
+    print(summary)
 
     if args.makeFigures == 1:
         make_figures(args, fepruns, dGs, mean, sterr)
