972b38cba8a756a6dbc1d9daa5fcc1f1d9bc7345
EzryStIago <ezrysm@gmail.com>
2025-11-26 16:34:45 -0500

    doc: add docstrings to existing EXP tests

--- safep/tests/test_exponential.py
+++ safep/tests/test_exponential.py
@@ -6,6 +6,11 @@ from safep import get_exponential
 
 
 def test_exponential_averaging():
+    """
+    Given a dummy dataset of all 1s
+    When exponential averaged
+    Expect all 1s
+    """
     u_nk = pd.read_csv(Path(__file__).parent/"test_u_nk.csv", index_col=(0,1))
     u_nk.columns = pd.to_numeric(u_nk.columns)
     l, l_mid, dG_f, dG_b = get_exponential(u_nk)
@@ -14,6 +19,11 @@ def test_exponential_averaging():
     assert np.allclose(dG_b, 1), f"dG_f:{dG_f[0]}, dG_b:{dG_b[0]}"
 
 def test_exponential_averaging_diff_fwd_bwd():
+    """
+    Given a dummy dataset of all 1s forward and -2s backward
+    When exponential averaged
+    Expect all 1s forward and -2s backward
+    """
     u_nk = pd.read_csv(Path(__file__).parent/"test_diff_fwd_bwd.csv", index_col=(0,1))
     u_nk.columns = pd.to_numeric(u_nk.columns)
     l, l_mid, dG_f, dG_b = get_exponential(u_nk)
