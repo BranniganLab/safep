1ae0175e4017997a8d7b2419900ba2584df4506c
EzryStIago <ezrysm@gmail.com>
2025-11-26 16:35:17 -0500

    test: Make an artificially bad window. The forward spike should come after the backward spike.

--- safep/tests/test_exponential.py
+++ safep/tests/test_exponential.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 from safep import get_exponential
-
+from alchemlyb.parsing import namd
 
 def test_exponential_averaging():
     """
@@ -30,3 +30,19 @@ def test_exponential_averaging_diff_fwd_bwd():
 
     assert np.allclose(dG_f, 1), f"dG_f:{dG_f[0]}, dG_b:{dG_b[0]}"
     assert np.allclose(dG_b, -2), f"dG_f:{dG_f[0]}, dG_b:{dG_b[0]}"
+
+def test_artificially_bad_window():
+    """
+    Given a dataset with an artificial bad window
+    When exponential averaged
+    Expect the forward spike to come after the backward spike
+    """
+    datapath = Path(__file__).parent/"../../Sample_Notebooks/Replica1"
+    u_nk = namd.extract_u_nk(list(datapath.glob("idws*.fepout")), 303.15)
+    u_nk = u_nk.sort_index(axis=0, level=1).sort_index(axis=1)
+    u_nk.loc[(slice(None), 0.3), slice(None)] *= 10
+    l, l_mid, dG_f, dG_b = get_exponential(u_nk)
+
+    outlier_forward = np.argmax(np.abs(dG_f))
+    outlier_backward = np.argmax(np.abs(dG_b))
+    assert outlier_forward == outlier_backward + 1, "Worst window misidentified"
