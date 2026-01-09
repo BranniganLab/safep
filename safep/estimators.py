fa1f46c51d79158471974b8d4fbf22dcc6192372
EzryStIago <ezrysm@gmail.com>
2026-01-09 12:52:37 -0500

    refactor: extract methods for estimators

--- safep/estimators.py
+++ safep/estimators.py
@@ -58,9 +58,9 @@ def get_BAR(bar):
     return l, l_mid, f, df, ddf, errors
 
 
-def do_estimation(u_nk, method="both"):
-    """
-    Run both exponential and BAR estimators and return the results in tidy dataframes.
+def do_estimation(u_nk: pd.DataFrame, method: str="both"):
+    """Run both exponential and BAR estimators and return the results in tidy dataframes.
+
     Arguments: u_nk in the alchemlyb format, method of fitting (String: BAR, EXP, or both)
     Returns: per_window estimates (including errors), cumulative estimates (including errors)
     """
@@ -68,38 +68,13 @@ def do_estimation(u_nk, method="both"):
     cumulative = pd.DataFrame()
     per_window = pd.DataFrame()
     if method in ["both", "BAR"]:
-        bar = BAR()
-        bar.fit(u_nk)
-        ls, l_mids, fs, dfs, ddfs, errors = get_BAR(bar)
-
-        bar_cumulative = pd.DataFrame()
-        bar_per_window = pd.DataFrame()
-
-        bar_cumulative[("BAR", "f")] = fs
-        bar_cumulative[("BAR", "errors")] = errors
-        bar_cumulative.index = ls
-
-        bar_per_window[("BAR", "df")] = dfs
-        bar_per_window[("BAR", "ddf")] = ddfs
-        bar_per_window.index = l_mids
+        bar_per_window, bar_cumulative = do_bar(u_nk)
 
         cumulative = pd.concat([cumulative, bar_cumulative], axis=1)
         per_window = pd.concat([per_window, bar_per_window], axis=1)
 
     if method in ["both", "EXP"]:
-        expl, expmid, dG_fs, dG_bs = get_exponential(u_nk)
-
-        exp_cumulative = pd.DataFrame()
-        exp_per_window = pd.DataFrame()
-
-        exp_cumulative[("EXP", "ff")] = np.insert(np.cumsum(dG_fs), 0, 0)
-        exp_cumulative[("EXP", "fb")] = np.insert(-np.cumsum(dG_bs), 0, 0)
-        exp_cumulative.index = expl
-
-        exp_per_window[("EXP", "dG_f")] = dG_fs
-        exp_per_window[("EXP", "dG_b")] = dG_bs
-        exp_per_window[("EXP", "difference")] = np.array(dG_fs) + np.array(dG_bs)
-        exp_per_window.index = expmid
+        exp_per_window, exp_cumulative = do_exp(u_nk)
 
         cumulative = pd.concat([cumulative, exp_cumulative], axis=1)
         per_window = pd.concat([per_window, exp_per_window], axis=1)
@@ -110,6 +85,52 @@ def do_estimation(u_nk, method="both"):
     cumulative = cumulative.fillna(0)
 
     return per_window.copy(), cumulative.copy()
+
+
+def do_exp(u_nk: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """Do exponential averaging and repackage the results into a more convenient dataframe.
+
+    Arguments: u_nk in the alchemlyb format
+    Returns: cumulative estimates (including errors), per window estimates (including errors)
+    """
+    expl, expmid, dG_fs, dG_bs = get_exponential(u_nk)
+
+    exp_cumulative = pd.DataFrame()
+    exp_cumulative[("EXP", "ff")] = np.insert(np.cumsum(dG_fs), 0, 0)
+    exp_cumulative[("EXP", "fb")] = np.insert(-np.cumsum(dG_bs), 0, 0)
+    exp_cumulative.index = expl
+
+    exp_per_window = pd.DataFrame()
+    exp_per_window[("EXP", "dG_f")] = dG_fs
+    exp_per_window[("EXP", "dG_b")] = dG_bs
+    exp_per_window[("EXP", "difference")] = np.array(dG_fs) + np.array(dG_bs)
+    exp_per_window.index = expmid
+
+    return exp_per_window, exp_cumulative
+
+
+def do_bar(u_nk: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """Do bar estimation and repackage the results into a more convenient dataframe.
+
+    Arguments: u_nk in the alchemlyb format
+    Returns: cumulative estimates (including errors), per window estimates (including errors)
+    """
+    bar = BAR()
+    bar.fit(u_nk)
+
+    ls, l_mids, fs, dfs, ddfs, errors = get_BAR(bar)
+
+    bar_cumulative = pd.DataFrame()
+    bar_cumulative[("BAR", "f")] = fs
+    bar_cumulative[("BAR", "errors")] = errors
+    bar_cumulative.index = ls
+
+    bar_per_window = pd.DataFrame()
+    bar_per_window[("BAR", "df")] = dfs
+    bar_per_window[("BAR", "ddf")] = ddfs
+    bar_per_window.index = l_mids
+
+    return bar_per_window, bar_cumulative
 
 
 def get_dG_from_data(data, temperature):
