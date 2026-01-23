91c598fd42777393394adfdb6bc6ea99f05a743e
EzryStIago <ezrysm@gmail.com>
2026-01-23 14:54:28 -0500

    test/fix: call to verify

--- safep/tests/test_rfep_parse.py
+++ safep/tests/test_rfep_parse.py
@@ -6,9 +6,9 @@ from pathlib import Path
 def test_RFEP_main_out():
     main(Path(__file__).parent/"RFEP_decouple.log")
     captured = capsys.readouterr()
-    verify captured.out
+    verify(captured.out)
 
 def test_RFEP_main_err():
     main(Path(__file__).parent / "RFEP_decouple.log")
     captured = capsys.readouterr()
-    verify captured.err
+    verify(captured.err)
