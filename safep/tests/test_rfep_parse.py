5b14e8c3024cb9a883b02e8b9997be6ec9c8ae91
EzryStIago <ezrysm@gmail.com>
2026-01-23 14:51:50 -0500

    test/fix: run RFEP main with RFEP_decouple.log

--- safep/tests/test_rfep_parse.py
+++ safep/tests/test_rfep_parse.py
@@ -1,12 +1,14 @@
 from approvaltests import verify
 import pytest
 from safep.RFEP_analysis import main
+from pathlib import Path
 
-pytest.mark.xfail
 def test_RFEP_main_out():
+    main(Path(__file__).parent/"RFEP_decouple.log")
     captured = capsys.readouterr()
     verify captured.out
 
 def test_RFEP_main_err():
+    main(Path(__file__).parent / "RFEP_decouple.log")
     captured = capsys.readouterr()
     verify captured.err
