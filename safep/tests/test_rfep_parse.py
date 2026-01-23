4200e987aa8edf1011d9b800ed0d0b4130ba9444
EzryStIago <ezrysm@gmail.com>
2026-01-23 15:00:33 -0500

    test/fix: use capsys fixtures

--- safep/tests/test_rfep_parse.py
+++ safep/tests/test_rfep_parse.py
@@ -3,12 +3,12 @@ import pytest
 from safep.RFEP_analysis import main
 from pathlib import Path
 
-def test_RFEP_main_out():
+def test_RFEP_main_out(capsys):
     main(Path(__file__).parent/"RFEP_decouple.log")
     captured = capsys.readouterr()
     verify(captured.out)
 
-def test_RFEP_main_err():
+def test_RFEP_main_err(capsys):
     main(Path(__file__).parent / "RFEP_decouple.log")
     captured = capsys.readouterr()
     verify(captured.err)
