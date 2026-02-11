73a5c234b98a9493d3d6f22326fffcdd373ca371
EzryStIago <ezrysm@gmail.com>
2026-02-11 13:20:19 -0500

    test/refactor: add traj fixture

--- safep/tests/test_TI.py
+++ safep/tests/test_TI.py
@@ -6,12 +6,14 @@ def test_read_namd_conf():
     config = read_namd_conf_moving_wall(Path(__file__).parent/"data/job_1.namd")
     assert config['stepsperstage'] == 5000000
 
-def test_read_colvars_traj():
-    traj = ColvarsTraj.read_colvars_traj(Path(__file__).parent/"data/pruned.colvars.traj")
-    assert traj.loc[19800, "DBC"] == 4.70510005898365e+00
+@pytest.fixture(scope="module")
+def pruned_traj() -> ColvarsTraj:
+    return ColvarsTraj.read_colvars_traj(Path(__file__).parent/"data/pruned.colvars.traj")
 
-def test_get_stages():
-    traj = ColvarsTraj.read_colvars_traj(Path(__file__).parent/"data/pruned.colvars.traj")
+def test_read_colvars_traj(pruned_traj):
+    assert pruned_traj.loc[19800, "DBC"] == 4.70510005898365e+00
+
+def test_get_stages(pruned_traj):
     config = {"stepsperstage": 1000, "stages": 20}
-    traj.get_stages(config)
-    assert traj.loc[19800, "stage"] == 19
+    pruned_traj.get_stages(config)
+    assert pruned_traj.loc[19800, "stage"] == 19
