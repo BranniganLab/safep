bc24cf2cb2c2c2d6e146aaad081dc433d2fa7cfb
EzryStIago <ezrysm@gmail.com>
2026-02-11 12:47:53 -0500

    feat: parse config file to dictionary

--- safep/moving_wall_TI.py
+++ safep/moving_wall_TI.py
@@ -7,5 +7,18 @@ def read_namd_conf_moving_wall(config: Path) -> dict:
     with open(config, encoding="UTF8") as f:
         lines = f.readlines()
     config = {}
+    for line in lines:
+        if "set" in line:
+            try:
+                _, key, value = line.strip().split(" ")
+                config[key] = value
+            except ValueError:
+                print(f"bad line: {line}")
+                continue
 
+    for key, value in config.items():
+        try:
+            config[key] = float(value)
+        except ValueError:
+            continue
     return config
