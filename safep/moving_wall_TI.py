5612024e0ea3ddfe1219c508a2341c7b0c3d5c1e
EzryStIago <ezrysm@gmail.com>
2026-02-11 13:04:50 -0500

    refactor: subclass pandas dataframe

--- safep/moving_wall_TI.py
+++ safep/moving_wall_TI.py
@@ -23,11 +23,36 @@ def read_namd_conf_moving_wall(config: Path) -> dict:
             continue
     return config
 
-def read_colvars_traj(traj_path: Path) -> pd.DataFrame:
-    with open(traj_path, encoding="UTF8") as f:
-        first_line = f.readline()
-    header = first_line.strip().split()[1:]
-    traj = pd.read_csv(traj_path, sep="\s+", header=None, engine="python", comment="#")
-    traj.columns = header
-    traj.set_index("step", inplace=True)
-    return traj
+class ColvarsTraj(pd.DataFrame):
+    def __init__(self):
+        super().__init__()
+
+    # Specify attributes to be carried over to new instances
+    _metadata = []
+
+    @property
+    def _constructor(self):
+        """preserve custom class after operations like group"""
+        return ColvarsTraj
+
+    @classmethod
+    def read_colvars_traj(cls, traj_path: Path) -> pd.DataFrame:
+        with open(traj_path, encoding="UTF8") as f:
+            first_line = f.readline()
+        header = first_line.strip().split()[1:]
+        traj = pd.read_csv(traj_path, sep="\s+", header=None, engine="python", comment="#")
+        traj.columns = header
+        traj.set_index("step", inplace=True)
+        return traj
+
+def get_stage_numbers(traj: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+def get_wall_position(traj: pd.DataFrame, config: dict) -> list:
+    steps_per_stage = config["stepsperstage"]
+    stages = config["stages"]
+    initial_wall = config["initialWall"]
+    final_wall = config["finalWall"]
+    steps = traj["step"]
+    stage_numbers = steps//steps_per_stage
+
--- safep/tests/test_TI.py
+++ safep/tests/test_TI.py
@@ -1,4 +1,4 @@
-from safep.moving_wall_TI import read_namd_conf_moving_wall, read_colvars_traj
+from safep.moving_wall_TI import read_namd_conf_moving_wall, ColvarsTraj
 from pathlib import Path
 
 def test_read_namd_conf():
@@ -6,5 +6,5 @@ def test_read_namd_conf():
     assert config['stepsperstage'] == 5000000
 
 def test_read_colvars_traj():
-    traj = read_colvars_traj(Path(__file__).parent/"data/pruned.colvars.traj")
+    traj = ColvarsTraj.read_colvars_traj(Path(__file__).parent/"data/pruned.colvars.traj")
     assert traj.loc[19800, "DBC"] == 4.70510005898365e+00
