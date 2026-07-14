82fc8e16bf8c9976964a8f2a0c151a5f2d37531d
EzryStIago <ezrysm@gmail.com>
2026-07-14 13:10:39 -0400

    test/fix: use tmp_path to avoid over-writing directories

--- safep/tests/test_afep_parse.py
+++ safep/tests/test_afep_parse.py
@@ -73,15 +73,15 @@ def test_sterr_of_two_numbers_propagates_error():
     assert not np.isclose(sterr, 0.5), "Got standard error. Standard error of two numbers is a math crime. The authorities have been informed."
     assert np.isclose(sterr, 2.236067977), "Error not propagated correctly."
 
-def test_cached_fepruns_match_expectations(fepruns: dict[FepRun]):
+def test_cached_fepruns_match_expectations(fepruns: dict[FepRun], tmp_path: Path):
     """
     Given a set of fepruns
     When written to file and read back
     Expect the feprun as-read to be identical to the feprun as-written
     """
     for key, fr in fepruns.items():
-        fr.to_dir(Path(key))
-        test_fr= FepRun.from_dir(Path(key))
+        fr.to_dir(tmp_path/key)
+        test_fr= FepRun.from_dir(tmp_path/key)
         for name in ["u_nk", "per_window", "cumulative", "forward", "forward_error", "backward", "backward_error", "per_lambda_convergence", "color"]:
             test = getattr(test_fr, name)
             canonical = getattr(fr, name)
